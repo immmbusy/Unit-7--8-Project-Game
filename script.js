75aedb103dfba35ecdfe9d8795dd511fd4426b58
Jian Q Liang <30308734+jqliang14@users.noreply.github.com>
2021-06-02 16:47:17 -0500

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ $("#submitButton").click(function() {
     let gender = $("#question1").val().toLowerCase();
     let description = parseInt($("#question2").val());
 
-    // Background imgs
     let background;
     let tang = "https://pbs.twimg.com/profile_images/1364238629707546624/vt7ms0in_400x400.jpg";
     let dai = "https://static.wikia.nocookie.net/soulland/images/e/e2/War_God.JPG/revision/latest?cb=20190509013800";
